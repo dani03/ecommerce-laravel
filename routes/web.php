66873ca953e094fad53159d9ce81e2c4cb14c618
daniel <lesamessipa@gmail.com>
2025-01-08 14:01:44 +0100

    add:filament installation for admin pannel

--- routes/web.php
+++ routes/web.php
@@ -10,13 +10,11 @@ Route::get('/', function () {
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    ]);
+})->name('dashboard');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
